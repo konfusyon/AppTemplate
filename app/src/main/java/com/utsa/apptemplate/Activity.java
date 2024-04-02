9a584a6394adb10007fd78b37920a5561a0672e9
DoggosAndCoffee <103789784+konfusyon@users.noreply.github.com>
2024-04-02 18:58:18 -0500

    App Template

---
+++ app/src/main/java/com/utsa/apptemplate/Activity.java
@@ -0,0 +1,26 @@
+package com.utsa.apptemplate;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.ComponentActivity;
+
+public class Activity extends ComponentActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.template);
+        buttonSetter();
+    }
+
+    private void buttonSetter() {
+        Button button = (Button) findViewById(R.id.login);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(getBaseContext(), "oop", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
